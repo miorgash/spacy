ea5af34a8bc949d66cf26690d4f9ef4321a572b1
miorgash <miorgash@gmail.com>
2020-09-10 00:23:44 +0000

    file_upload and app.upload_multipart() worked

--- spacy/app.py
+++ spacy/app.py
@@ -1,5 +1,5 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask, request, make_response, render_template, jsonify
+from flask import Flask, request, make_response, render_template, jsonify, redirect, url_for
 import os
 
 app = Flask(__name__)
@@ -7,7 +7,20 @@ UPLOAD_DIR = '/nlp-data/uploaded'
 
 @app.route("/")
 def home():
-    return render_template("text_upload.html")
+    return render_template("upload.html")
+
+@app.route("/data/upload", methods=['POST'])
+def upload_multipart():
+    # TODO: RESTful に変更
+    file_ = request.files['uploadFile']
+    fileName = file_.filename
+
+    file_.save(os.path.join(UPLOAD_DIR, fileName))
+    return redirect(url_for("thanks")) # 関数名で指定
+
+@app.route("/thanks")
+def thanks():
+    return render_template("thanks.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5109)
+    app.run(host='0.0.0.0', port=5108)
